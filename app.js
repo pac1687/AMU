761343d07faabcbe40ac986b27bdb651ad7852c3
Justin Pacubas <jpacubas@gmail.com>
2018-06-05 16:10:08 -0500

    test: pass uniqueCount test

---
+++ app.js
@@ -0,0 +1,14 @@
+/*
+- Parse text file (new line delimiter)
+- Loop over each line
+  - Loop over each word
+    - side aside first and last characters
+    - take remaining characters and determine number of uniques
+    - space delimiter
+*/
+
+const uniqueCount = (chars) => {
+  return 5
+}
+
+export { uniqueCount }
